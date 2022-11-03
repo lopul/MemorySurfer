9c145352e82a1ec1aa9e39c26e9a1c16c6941c84
Lorenz Pullwitt <68168097+lopul@users.noreply.github.com>
2022-11-03 14:58:39 +0100

    Add files via upload

--- memorysurfer.c
+++ memorysurfer.c
@@ -43,9 +43,9 @@
 #include <fcntl.h> // O_TRUNC / O_EXCL
 #include <errno.h>
 
-static const int32_t MSF_VERSION = 0x010001c1;
+static const int32_t MSF_VERSION = 0x010001c2;
 
-enum Error { E_OK, E_FAIL, E_UNESC = 0x012cf4b0, E_PXML = 0x0025968a, E_CRRPT = 0x0687f5d6, E_ASSRT_1 = 0x068e1507, E_HEX = 0x0002b106, E_POST = 0x003e3ed8, E_RPOFT = 0x115048c5, E_FIELD_1 = 0x0169002d, E_FIELD_2 = 0x0169002e, E_FIELD_3 = 0x0169002f, E_FIELD_4 = 0x01690030, E_FIELD_5 = 0x01690031, E_FIELD_6 = 0x01690032, E_FIELD_7 = 0x01690033, E_PARSE_1 = 0x01d087cf, E_PARSE_2 = 0x01d087d0, E_MISMA = 0x007a49be, E_PARSE_4 = 0x01d087d2, E_PARSE_3 = 0x01d087d1, E_EXPOR_1 = 0x05e29399, E_EXPOR_2 = 0x05e2939a, E_EXPOR_3 = 0x05e2939b, E_GHTML_1 = 0x03f6667d, E_GHTML_2 = 0x03f6667e, E_GHTML_3 = 0x03f6667f, E_GHTML_4 = 0x03f66680, E_GHTML_5 = 0x03f66681, E_GHTML_6 = 0x03f66682, E_GENLRN_1 = 0x7d95d699, E_GENLRN_2 = 0x7d95d69a, E_GENLRN_3 = 0x7d95d69b, E_GENLRN_4 = 0x7d95d69c, E_GENLRN_5 = 0x7d95d69d, E_GENLRN_6 = 0x7d95d69e, E_GENLRN_7 = 0x7d95d69f, E_GENLRN_8 = 0x7d95d6a0, E_GENLRN_9 = 0x7d95d6a1, E_GHTML_7 = 0x03f66683, E_GHTML_8 = 0x03f66684, E_GHTML_9 = 0x03f66685, E_MALLOC_1 = 0x1e8e2971, E_MALLOC_2 = 0x1e8e2972, E_MALLOC_3 = 0x1e8e2973, E_ARG_1 = 0x0000da5d, E_ASSRT_2 = 0x0000da5d, E_DETECA = 0x099201b8, E_ARG_2 = 0x0000da5e, E_MALLOC_4 = 0x1e8e2974, E_MALLOC_5 = 0x1e8e2975, E_INIT = 0x003d20c0, E_ASSRT_3 = 0x068e1509, E_ASSRT_4 = 0x068e150a, E_CARD_1 = 0x000e0539, E_CARD_2 = 0x000e053a, E_CARD_3 = 0x000e053b, E_CARD_4 = 0x000e053c, E_DECK_1 = 0x00216467, E_DECK_2 = 0x00216468, E_DECK_3 = 0x00216469, E_DECK_4 = 0x0021646a, E_ASSRT_5 = 0x068e150b, E_UPLOAD_1 = 0x22b56c8f, E_ARRANG_1 = 0x4052a587, E_ARRANG_2 = 0x4052a588, E_CARD_5 = 0x000e053d, E_CARD_6 = 0x000e053e, E_CARD_7 = 0x000e053f, E_LVL_1 = 0x00016d65, E_CARD_8 = 0x000e0540, E_CARD_9 = 0x000e0541 };
+enum Error { E_OK, E_FAIL, E_UNESC = 0x012cf4b0, E_PXML = 0x0025968a, E_CRRPT = 0x0687f5d6, E_ASSRT_1 = 0x068e1507, E_HEX = 0x0002b106, E_POST = 0x003e3ed8, E_RPOFT = 0x115048c5, E_FIELD_1 = 0x0169002d, E_FIELD_2 = 0x0169002e, E_FIELD_3 = 0x0169002f, E_FIELD_4 = 0x01690030, E_FIELD_5 = 0x01690031, E_FIELD_6 = 0x01690032, E_FIELD_7 = 0x01690033, E_PARSE_1 = 0x01d087cf, E_HASH_1 = 0x001a255d, E_HASH_2 = 0x001a255e, E_PARSE_2 = 0x01d087d0, E_MISMA = 0x007a49be, E_PARSE_4 = 0x01d087d2, E_PARSE_3 = 0x01d087d1, E_EXPOR_1 = 0x05e29399, E_EXPOR_2 = 0x05e2939a, E_EXPOR_3 = 0x05e2939b, E_GHTML_1 = 0x03f6667d, E_GHTML_2 = 0x03f6667e, E_GHTML_3 = 0x03f6667f, E_GHTML_4 = 0x03f66680, E_GHTML_5 = 0x03f66681, E_GHTML_6 = 0x03f66682, E_GENLRN_1 = 0x7d95d699, E_GENLRN_2 = 0x7d95d69a, E_GENLRN_3 = 0x7d95d69b, E_GENLRN_4 = 0x7d95d69c, E_GENLRN_5 = 0x7d95d69d, E_GENLRN_6 = 0x7d95d69e, E_GENLRN_7 = 0x7d95d69f, E_GENLRN_8 = 0x7d95d6a0, E_GENLRN_9 = 0x7d95d6a1, E_GHTML_7 = 0x03f66683, E_GHTML_8 = 0x03f66684, E_GHTML_9 = 0x03f66685, E_MALLOC_1 = 0x1e8e2971, E_MALLOC_2 = 0x1e8e2972, E_MALLOC_3 = 0x1e8e2973, E_ARG_1 = 0x0000da5d, E_ASSRT_2 = 0x0000da5d, E_DETECA = 0x099201b8, E_ARG_2 = 0x0000da5e, E_MALLOC_4 = 0x1e8e2974, E_MALLOC_5 = 0x1e8e2975, E_INIT = 0x003d20c0, E_ASSRT_3 = 0x068e1509, E_ASSRT_4 = 0x068e150a, E_CARD_1 = 0x000e0539, E_CARD_2 = 0x000e053a, E_CARD_3 = 0x000e053b, E_CARD_4 = 0x000e053c, E_DECK_1 = 0x00216467, E_DECK_2 = 0x00216468, E_DECK_3 = 0x00216469, E_DECK_4 = 0x0021646a, E_ASSRT_5 = 0x068e150b, E_UPLOAD_1 = 0x22b56c8f, E_ARRANG_1 = 0x4052a587, E_ARRANG_2 = 0x4052a588, E_CARD_5 = 0x000e053d, E_CARD_6 = 0x000e053e, E_CARD_7 = 0x000e053f, E_LVL_1 = 0x00016d65, E_CARD_8 = 0x000e0540, E_CARD_9 = 0x000e0541 };
 enum Field { F_UNKNOWN, F_FILE_TITLE, F_UPLOAD, F_ARRANGE, F_CAT_NAME, F_STYLE_TXT, F_MOVED_CAT, F_SEARCH_TXT, F_MATCH_CASE, F_IS_HTML, F_IS_UNLOCKED, F_CAT, F_CARD, F_MOV_CARD, F_LVL, F_RANK, F_Q, F_A, F_REVEAL_POS, F_TODO_MAIN, F_TODO_ALT, F_MTIME, F_PASSWORD, F_NEW_PASSWORD, F_TOKEN, F_EVENT, F_PAGE, F_MODE, F_TIMEOUT };
 enum Action { A_END, A_NONE, A_FILE, A_WARN_UPLOAD, A_CREATE, A_NEW, A_OPEN_DLG, A_FILELIST, A_OPEN, A_CHANGE_PASSWD, A_WRITE_PASSWD, A_READ_PASSWD, A_CHECK_PASSWORD, A_AUTH_PASSWD, A_AUTH_TOK, A_GEN_TOK, A_LOAD_CARDLIST, A_LOAD_CARDLIST_OLD, A_GET_CARD, A_CHECK_RESUME, A_SLASH, A_VOID, A_FILE_EXTENSION, A_GATHER, A_UPLOAD, A_UPLOAD_REPORT, A_EXPORT, A_ASK_REMOVE, A_REMOVE, A_ASK_ERASE, A_ERASE, A_CLOSE, A_START_DECKS, A_DECKS_CREATE, A_SELECT_DEST_DECK, A_SELECT_SEND_CAT, A_SELECT_ARRANGE, A_CAT_NAME, A_STYLE_GO, A_CREATE_DECK, A_RENAME_CAT, A_READ_STYLE, A_STYLE_APPLY, A_ASK_DELETE_DECK, A_DELETE_CAT, A_TOGGLE, A_MOVE_CAT, A_SELECT_EDIT_CAT, A_EDIT, A_UPDATE_QA, A_UPDATE_QA_OLD, A_UPDATE_HTML, A_SYNC, A_INSERT, A_APPEND, A_ASK_DELETE_CARD, A_DELETE_CARD, A_PREVIOUS, A_NEXT, A_SCHEDULE, A_SET, A_CARD_ARRANGE, A_MOVE_CARD, A_SEND_CARD, A_SELECT_LEARN_CAT, A_SELECT_SEARCH_CAT, A_PREFERENCES, A_ABOUT, A_APPLY, A_SEARCH, A_PREVIEW, A_RANK, A_DETERMINE_CARD, A_SHOW, A_REVEAL, A_PROCEED, A_ASK_SUSPEND, A_SUSPEND, A_ASK_RESUME, A_RESUME, A_CHECK_FILE, A_LOGIN, A_HISTOGRAM, A_TABLE, A_RETRIEVE_MTIME, A_MTIME_TEST, A_TEST_CARD, A_TEST_CAT_SELECTED, A_TEST_CAT_VALID, A_TEST_DECK, A_TEST_ARRANGE, A_TEST_NAME };
 enum Page { P_UNDEF = -1, P_START, P_FILE, P_PASSWORD, P_NEW, P_OPEN, P_UPLOAD, P_UPLOAD_REPORT, P_EXPORT, P_CAT_NAME, P_STYLE, P_SELECT_ARRANGE, P_SELECT_DEST_DECK, P_SELECT_DECK, P_EDIT, P_PREVIEW, P_SEARCH, P_PREFERENCES, P_ABOUT, P_LEARN, P_MSG, P_HISTOGRAM, P_TABLE };
@@ -306,6 +306,7 @@ struct WebMemorySurfer {
   char tok_str[41];
   struct IndentStr *inds;
   char *temp_filename;
+  uint8_t *posted_message_digest;
   int card_n;
   int deck_n;
 };
@@ -1910,7 +1911,7 @@ static int parse_post(struct WebMemorySurfer *wms)
   int rv; // return value
   char *str;
   enum Stage stage;
-  int len;
+  size_t len;
   size_t size;
   struct Multi *mult;
   char *boundary_str;
@@ -1921,12 +1922,9 @@ static int parse_post(struct WebMemorySurfer *wms)
   int cmp_i;
   int post_tp;
   struct Parse *parse;
-  uint8_t posted_message_digest[SHA1_HASH_SIZE];
   uint8_t message_digest[SHA1_HASH_SIZE];
-  int have_sha1;
   struct Sha1Context sha1;
   boundary_str = NULL;
-  have_sha1 = 0;
   size = sizeof(struct Multi);
   mult = malloc(size);
   size = sizeof(struct Parse);
@@ -2108,19 +2106,25 @@ static int parse_post(struct WebMemorySurfer *wms)
             mult->post_lp[mult->post_fp] = '\0';
             str = strrchr(mult->post_lp, '.');
             i = str - mult->post_lp;
+            assert(i > 0 && mult->post_fp >= i);
             len = mult->post_fp - i;
             e = str == NULL || len != 4 || strncmp(str, ".xml", 4) != 0;
             if (e == 0) {
               str = strrchr(mult->post_lp, '#');
               if (str != NULL && strncmp(str, "#sha1-", 6) == 0) {
                 j = str - mult->post_lp + 6;
+                assert(i >= j);
                 len = i - j;
                 size = SHA1_HASH_SIZE * 2;
-                e = len < size;
+                e = len < size ? E_HASH_1 : 0;
                 if (e == 0) {
-                  e = scan_hex(posted_message_digest, mult->post_lp + j, SHA1_HASH_SIZE);
+                  e = wms->posted_message_digest != NULL ? E_HASH_2 : 0;
                   if (e == 0) {
-                    have_sha1 = 1;
+                    wms->posted_message_digest = malloc(SHA1_HASH_SIZE);
+                    e = wms->posted_message_digest == NULL;
+                    if (e == 0) {
+                      e = scan_hex(wms->posted_message_digest, mult->post_lp + j, SHA1_HASH_SIZE);
+                    }
                   }
                 }
               }
@@ -2199,7 +2203,7 @@ static int parse_post(struct WebMemorySurfer *wms)
               }
             }
           }
-          if (have_sha1 == 1 && e == 0) {
+          if (wms->posted_message_digest != NULL && e == 0) {
             e = sha1_reset(&sha1);
             if (e == 0) {
               temp_stream = fopen(wms->temp_filename, "r");
@@ -2215,7 +2219,7 @@ static int parse_post(struct WebMemorySurfer *wms)
                 if (e == 0) {
                   e = sha1_result(&sha1, message_digest);
                   if (e == 0) {
-                    e = memcmp(posted_message_digest, message_digest, 20) != 0 ? E_MISMA : 0;
+                    e = memcmp(wms->posted_message_digest, message_digest, 20) != 0 ? E_MISMA : 0;
                   }
                 }
                 rv = fclose(temp_stream);
@@ -3516,7 +3520,7 @@ static int gen_html(struct WebMemorySurfer *wms)
               wms->ms.card_i >= 0 && wms->ms.card_a > 0 && wms->ms.card_i < wms->ms.card_a && (wms->ms.card_l[wms->ms.card_i].card_state & 0x07) >= STATE_NEW ? "" : " disabled",
               wms->ms.card_i != wms->ms.mov_card_i ? "" : " disabled",
               wms->ms.mov_card_i != -1 && wms->ms.card_i != wms->ms.mov_card_i ? "" : " disabled",
-              wms->ms.card_i != -1 && n == 2 ? "" : " disabled");
+              wms->ms.card_i != -1 && n > 1 ? "" : " disabled");
           e = rv < 0;
           if (e == 0) {
             e = xml_escape(&wms->html_lp, &wms->html_n, a_str, ESC_AMP | ESC_LT);
@@ -4192,6 +4196,7 @@ static int wms_init(struct WebMemorySurfer *wms)
         if (e == 0) {
           inds_init(wms->inds);
           wms->temp_filename = NULL;
+          wms->posted_message_digest = NULL;
         }
       }
     }
@@ -4235,8 +4240,11 @@ static void inds_free(struct IndentStr *inds)
   inds->indent_n = -1;
 }
 
-static void wms_free(struct WebMemorySurfer *wms) {
+static void wms_free(struct WebMemorySurfer *wms)
+{
   int i;
+  free(wms->posted_message_digest);
+  wms->posted_message_digest = NULL;
   free(wms->temp_filename);
   wms->temp_filename = NULL;
   inds_free(wms->inds);
@@ -5204,8 +5212,9 @@ int main(int argc, char *argv[])
                         }
                       }
                       need_sync = e == 0;
-                      if (e == 0)
+                      if (e == 0) {
                         wms->page = P_UPLOAD_REPORT;
+                      }
                     }
                   }
                 }
